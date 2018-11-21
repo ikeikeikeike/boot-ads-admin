ee5bc8f27799f03e9caf35a36017c2dc6439c3f1
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-11-21 23:09:00 +0900

    show article link at django admin

--- admin/ads/core/admin.py
+++ admin/ads/core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import html
 
 from imagekit.admin import AdminThumbnail
 
@@ -41,8 +42,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'user', 'category', 'tag_list', 'admin_image', 'external_link', 'slug',
-        'title', 'publish', 'publish_at', 'created_at', 'updated_at'
+        'id', 'user', 'category', 'tag_list', 'admin_image', 'external_tag',
+        'slug_tag', 'title', 'publish', 'publish_at', 'created_at', 'updated_at'
     )
     list_filter = (
         'created_at', 'updated_at', 'publish_at',
@@ -75,6 +76,22 @@ class PostAdmin(admin.ModelAdmin):
     def tag_list(self, obj):
         return u", ".join(o.name for o in obj.tags.all())
     tag_list.short_description = "tags"
+
+    def external_tag(self, obj):
+        if obj.external_link:
+            a = '<a href="{0.external_link}" target="_blank">{0.external_link}</a>'.format(obj)
+            return html.format_html(a)
+        return ""
+    external_tag.allow_tags = True
+    external_tag.short_description = "show external page"
+
+    def slug_tag(self, obj):
+        if obj.slug:
+            a = '<a href="/articles/{0.slug}" target="_blank">{0.slug}</a>'.format(obj)
+            return html.format_html(a)
+        return ""
+    slug_tag.allow_tags = True
+    slug_tag.short_description = "show article page"
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
